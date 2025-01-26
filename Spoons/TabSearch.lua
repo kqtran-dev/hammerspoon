052ba9805400c1f73a9df099bb839a84c9657339
k <k@Mac.socal.rr.com>
2025-01-25 16:49:51 -0800

    add numi shortcut

--- Spoons/TabSearch.lua
+++ Spoons/TabSearch.lua
@@ -3,15 +3,9 @@ local TabSearch = {}
 function TabSearch.trigger()
     local frontApp = hs.application.frontmostApplication()
     if frontApp:name() == "Firefox" then
-        -- hs.alert("Triggering Tab Search in Firefox!")
-        -- 
         -- Focus the address bar
         hs.eventtap.keyStroke({"cmd"}, "l")  -- Cmd+L equivalent to focus address bar
-
-        -- Small delay before typing %
-        -- hs.timer.doAfter(0.2, function()
-            hs.eventtap.keyStrokes("% ")
-        -- end)
+        hs.eventtap.keyStrokes("% ")
     else
         -- hs.alert("This shortcut only works in Firefox!")
     end
--- init.lua
+++ init.lua
@@ -4,6 +4,12 @@ local TabSearch = require("Spoons.tabsearch")
 -- Bind the TabSearch trigger to Ctrl+Shift+A
 hs.hotkey.bind({"ctrl", "shift"}, "A", TabSearch.trigger)
 
+-- Load the NumiMinimize Spoon
+hs.loadSpoon("NumiMinimize")
+
+-- Bind the hotkey using the Spoon
+spoon.NumiMinimize:bindHotkey()
+
 -- Notify that Hammerspoon is ready
 hs.alert("Hammerspoon Config Loaded!")
 
